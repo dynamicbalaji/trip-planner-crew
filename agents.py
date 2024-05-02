ff1fcf2aa166afeb70cf814ac1adcc9c6c3b2fc8
Balaji Ashok Kumar (BK) <dynamicbalaji4u@gmail.com>
2024-05-02 16:57:32 -0400

    :tada: Add 3 agents for trip planning

--- agents.py
+++ agents.py
@@ -3,6 +3,33 @@ from textwrap import dedent
 from langchain.llms import OpenAI, Ollama
 from langchain_openai import ChatOpenAI
 
+"""
+Creating Agents Cheat Sheet:
+- Think like a boss. Work backwards from the goal and think which employee 
+    you need to hire to get the job done.
+- Define the Captain of the crew who orient the other agents towards the goal. 
+- Define which experts the captain needs to communicate with and delegate tasks to.
+    Build a top down structure of the crew.
+
+Goal:
+- Create a 7-day travel itinerary with detailed per-day plans,
+    including budget, packing suggestions, and safety tips.
+
+Captain/Manager/Boss:
+- Expert Travel Agent
+
+Employees/Experts to hire:
+- City Selection Expert 
+- Local Tour Guide
+
+
+Notes:
+- Agents should be results driven and have a clear goal in mind
+- Role is their job title
+- Goals should actionable
+- Backstory should be their resume
+"""
+
 
 # This is an example of how to define custom agents.
 # You can define as many agents as you want.
@@ -13,24 +40,41 @@ class CustomAgents:
         self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
         self.Ollama = Ollama(model="openhermes")
 
-    def agent_1_name(self):
+    def expert_travel_agent(self):
         return Agent(
-            role="Define agent 1 role here",
-            backstory=dedent(f"""Define agent 1 backstory here"""),
-            goal=dedent(f"""Define agent 1 goal here"""),
+            role="Expert Travel Agent",
+            goal=dedent(f"""
+                        Create a 7-day travel itinerary with detailed per-day plans,
+                        include budget, packing suggestions, and safety tips.
+                        """),
+            backstory=dedent(
+                f"""Expert in travel planning and logistics. 
+                I have decades of expereince making travel iteneraries."""),
             # tools=[tool_1, tool_2],
-            allow_delegation=False,
             verbose=True,
             llm=self.OpenAIGPT35,
         )
 
-    def agent_2_name(self):
+    def city_selection_expert(self):
         return Agent(
-            role="Define agent 2 role here",
-            backstory=dedent(f"""Define agent 2 backstory here"""),
-            goal=dedent(f"""Define agent 2 goal here"""),
+            role="City Selection Expert",
+            goal=dedent(
+                f"""Select the best cities based on weather, season, prices, and traveler interests"""),
+            backstory=dedent(
+                f"""Expert at analyzing travel data to pick ideal destinations"""),
             # tools=[tool_1, tool_2],
-            allow_delegation=False,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
+
+    def local_tour_guide(self):
+        return Agent(
+            role="Local Tour Guide",
+            goal=dedent(
+                f"""Provide the BEST insights about the selected city"""),
+            backstory=dedent(f"""Knowledgeable local guide with extensive information
+        about the city, it's attractions and customs"""),
+            # tools=[tool_1, tool_2],
             verbose=True,
             llm=self.OpenAIGPT35,
         )
